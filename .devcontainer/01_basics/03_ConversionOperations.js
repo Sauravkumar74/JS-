b7495a8526a8028ac0ae392701e5d55d95230bf2
Sauravkumar74 <iamsauravkumar74@gmail.com>
2024-10-01 16:31:44 +0000

    conversion of datatypes done

---
+++ .devcontainer/01_basics/03_ConversionOperations.js
@@ -0,0 +1,27 @@
+//let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueINNumber = Number(score)
+//console.log(typeof valueINNumber);
+//console.log(valueINNumber);
+
+//"33" => 33
+// "33abc"=> NaN
+//true => 1; false => 0
+ 
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin);
+//1 =>true; 0=> false
+//""=> false
+//"saurav"=> true
+
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
