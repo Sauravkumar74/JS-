24fd621f4d5d74cf0609efb852cbe927ca64a1da
Sauravkumar74 <iamsauravkumar74@gmail.com>
2024-10-01 15:57:28 +0000

    config done

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -4,3 +4,24 @@ console.log(3+3);
 console.log("saurav");
 /* note: code readability should be high*/
 
+
+let name = "saurav"
+let age = 20
+let isLoggedIN = false
+
+//number => 2 to  power 53
+//BigInt
+//string => ""
+// boolean => true/false
+//null => standalone value
+//undefined=>
+//symbol => unique
+
+//object
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
+
